f06b8a350059c44af861614effd97955defa568f
Karthik007 <karthik.krao1999@gmail.com>
2020-10-19 01:00:44 +0530

    Updates

---
+++ precache-manifest.a16c10e9590e42b9bb2e7a67069cc87c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba986ae1ba4df423a7d1c480c57440a3",
+    "url": "/to-do-list.git/index.html"
+  },
+  {
+    "revision": "209dd1e08f343e5231a3",
+    "url": "/to-do-list.git/static/css/main.4d8ab585.chunk.css"
+  },
+  {
+    "revision": "98cfba505e20897d51d1",
+    "url": "/to-do-list.git/static/js/2.517b139e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-do-list.git/static/js/2.517b139e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209dd1e08f343e5231a3",
+    "url": "/to-do-list.git/static/js/main.778103ff.chunk.js"
+  },
+  {
+    "revision": "96e477f1f930980be0dc",
+    "url": "/to-do-list.git/static/js/runtime-main.17887af4.js"
+  }
+]);
